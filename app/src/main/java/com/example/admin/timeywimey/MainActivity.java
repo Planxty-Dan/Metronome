2c35c2bb05813621f40dbe3b2140317e4ad284be
Planxty-Dan <dan.newport@detroitlabs.com>
2014-10-16 16:32:29 -0400

    rewired button to start a countDownTimer with a delay between playing sound clip, now works as a metronome

--- app/src/main/java/com/example/admin/timeywimey/MainActivity.java
+++ app/src/main/java/com/example/admin/timeywimey/MainActivity.java
@@ -4,11 +4,16 @@ import android.app.Activity;
 import android.content.Context;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.logging.LogRecord;
 
 
 public class MainActivity extends Activity {
@@ -17,26 +22,38 @@ public class MainActivity extends Activity {
     private TextView mBeatCount;
     private int mBeatCounter = 0;
     private MediaPlayer mp;
+    private CountDownTimer mCountDownTimer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mp = MediaPlayer.create(getApplicationContext(), R.raw.wood_block);
         mSoundButton = (Button) findViewById(R.id.sound_button);
         mSoundButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mp.start();
-                mBeatCounter++;
-                mBeatCount.setText(Integer.toString(mBeatCounter));
+                playBeat();
             }
         });
         mBeatCount = (TextView) findViewById(R.id.beat_count);
         mBeatCount.setText(Integer.toString(mBeatCounter));
     }
 
+    public void playBeat() {
+       mCountDownTimer = new CountDownTimer(60000, 500) {
+           @Override
+           public void onTick(long l) {
+                mp.start();
+           }
+
+           @Override
+           public void onFinish() {
+
+           }
+       }.start();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
